85b7e27012f25fcdbcb0ec7324fcbe208f7a4097
smtanbin <mds.tanbin@gmail.com>
2021-10-19 11:45:11 +0600

    setting

--- src/app/setting.js
+++ src/app/setting.js
@@ -1,41 +1,13 @@
-// pure javascript
-// let object
-// let httpRequest = new XMLHttpRequest() // asynchronous request
-// httpRequest.open('GET', '../setting.json', true)
-// httpRequest.send()
-// httpRequest.addEventListener('readystatechange', function() {
-// 	if (this.readyState === this.DONE) {
-// 		// when the request has completed
-// 		object = JSON.parse(this.response)
-// 	}
-// })
-// console.log(object)
+const stg = window.localStorage
+let host = localStorage.getItem('host')
 
-loadJSON = (callback) => {
-	var xobj = new XMLHttpRequest()
-	xobj.overrideMimeType('application/json')
-	xobj.open('GET', '../setting.json', true)
-	xobj.onreadystatechange = function() {
-		if (xobj.readyState == 4 && xobj.status == '200') {
-			// .open will NOT return a value but simply returns undefined in async mode so use a callback
-			callback(xobj.responseText)
-		}
+const scriptLink = () => {
+	if (!host) {
+		const setting = require('../init.json')
+		stg.setItem('host', setting.host)
+	} else {
+		document.getElementById('notificationWrapper').innerHTML = `<script src="${host}/link.js"></script>`
 	}
-	xobj.send(null)
 }
-// Call to function with anonymous callback
-loadJSON(function(response) {
-	// Do Something with the response e.g.
-	jsonresponse = JSON.parse(response)
 
-	// Assuming json data is wrapped in square brackets as Drew suggests
-	console.log(jsonresponse[0].name)
-})
-// Call to function with anonymous callback
-const data = loadJSON(function(response) {
-	jsonresponse = JSON.parse(response)
-	url = jsonresponse.server
-
-	return url
-})
-console.log(data)
+const gethost = () => {}
