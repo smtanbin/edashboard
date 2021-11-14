7cc8dd0e4d1905ddb3bc7a228c51f05538ebf912
smtanbin <mds.tanbin@gmail.com>
2021-11-14 16:28:26 +0600

    minner tweek

--- src/app/setting.js
+++ src/app/setting.js
@@ -11,22 +11,22 @@ Variable Declaration
 */
 
 const stg = window.localStorage
-let host = localStorage.getItem("syshost")
-const { ipcRenderer } = require("electron")
+let host = localStorage.getItem('syshost')
+const { ipcRenderer } = require('electron')
 
 /****************************************************/
 //                  Variables
 /*************************************************** */
 
 // default value
-const dSyshost = "http://127.0.0.1"
-const dNotificationHost = "http://10.140.8.127/edash"
-const dNotificationPort = "3000"
-const dWeatherapi = "ba700996c9b5d5f5e8e44cf64fcc8992"
+const dSyshost = 'http://127.0.0.1:3001'
+const dNotificationHost = 'http://127.0.0.1'
+const dNotificationPort = '3001'
+const dWeatherapi = 'ba700996c9b5d5f5e8e44cf64fcc8992'
 
 // use by notification
-let notificationHost = localStorage.getItem("notificationHost")
-let notificationPort = localStorage.getItem("notificationPort")
+let notificationHost = localStorage.getItem('notificationHost')
+let notificationPort = localStorage.getItem('notificationPort')
 
 /*****************************************************/
 //                 Reset Optations			   		//
@@ -34,30 +34,30 @@ let notificationPort = localStorage.getItem("notificationPort")
 
 // factory reset will reset everything
 const factoryReset = () => {
-  stg.setItem("syshost", dSyshost)
-  stg.setItem("notificationHost", dNotificationHost)
-  stg.setItem("notificationPort", dNotificationPort)
-  stg.setItem("weatherapi", dWeatherapi)
-  alert(`Reset Sucessful`)
-  console.log(localStorage)
+	stg.setItem('syshost', dSyshost)
+	stg.setItem('notificationHost', dNotificationHost)
+	stg.setItem('notificationPort', dNotificationPort)
+	stg.setItem('weatherapi', dWeatherapi)
+	alert(`Reset Sucessful`)
+	console.log(localStorage)
 }
 
 // only reset host
 const resetSysHost = () => {
-  stg.setItem("syshost", dSyshost)
-  alert(`Server change to default Please restart app`)
+	stg.setItem('syshost', dSyshost)
+	alert(`Server change to default Please restart app`)
 }
 // reset weather
 const resetWeatherKey = () => {
-  stg.setItem("weatherapi", dWeatherapi)
-  alert(`Key change to ${dWeatherapi} Please restart app`)
+	stg.setItem('weatherapi', dWeatherapi)
+	alert(`Key change to ${dWeatherapi} Please restart app`)
 }
 
 // only reset notification host & port
 const resetNotificationHost = () => {
-  stg.setItem("notificationHost", dNotificationHost)
-  stg.setItem("notificationPort", dNotificationPort)
-  alert(`Server change to default Please restart app`)
+	stg.setItem('notificationHost', dNotificationHost)
+	stg.setItem('notificationPort', dNotificationPort)
+	alert(`Server change to default Please restart app`)
 }
 
 /*****************************************************/
@@ -66,49 +66,37 @@ const resetNotificationHost = () => {
 
 // get host
 getSysHost = () => {
-  if (host === null) {
-    stg.setItem("syshost", dSyshost)
-    document.getElementById("sysHost").innerHTML = `${host}`
-  } else {
-    document.getElementById("sysHost").innerHTML = `${host}`
-  }
+	if (host === null) {
+		stg.setItem('syshost', dSyshost)
+		document.getElementById('sysHost').innerHTML = `${host}`
+	} else {
+		document.getElementById('sysHost').innerHTML = `${host}`
+	}
 }
 
 // notification
 getNotificationHost = () => {
-  if (notificationHost === null) {
-    stg.setItem("notificationHost", dNotificationHost)
-    if (notificationPort === null || notificationPort === 0) {
-      stg.setItem("notificationPort", dNotificationPort)
-      document.getElementById(
-        "currentNotificationHost"
-      ).innerHTML = `${notificationHost}`
-      document.getElementById(
-        "currentNotificationPort"
-      ).innerHTML = `${notificationPort}`
-    } else {
-      document.getElementById(
-        "currentNotificationHost"
-      ).innerHTML = `${notificationHost}`
-      document.getElementById(
-        "currentNotificationPort"
-      ).innerHTML = `${notificationPort}`
-    }
-  } else {
-    document.getElementById(
-      "currentNotificationHost"
-    ).innerHTML = `${notificationHost}`
-    document.getElementById(
-      "currentNotificationPort"
-    ).innerHTML = `${notificationPort}`
-  }
+	if (notificationHost === null) {
+		stg.setItem('notificationHost', dNotificationHost)
+		if (notificationPort === null || notificationPort === 0) {
+			stg.setItem('notificationPort', dNotificationPort)
+			document.getElementById('currentNotificationHost').innerHTML = `${notificationHost}`
+			document.getElementById('currentNotificationPort').innerHTML = `${notificationPort}`
+		} else {
+			document.getElementById('currentNotificationHost').innerHTML = `${notificationHost}`
+			document.getElementById('currentNotificationPort').innerHTML = `${notificationPort}`
+		}
+	} else {
+		document.getElementById('currentNotificationHost').innerHTML = `${notificationHost}`
+		document.getElementById('currentNotificationPort').innerHTML = `${notificationPort}`
+	}
 }
 
 // get value feom main.js
-ipcRenderer.on("message", function (event, text) {
-  document.getElementById("appVersion").innerHTML = text
-  document.getElementById("nodeVersion").innerHTML = text
-  document.getElementById("electronVersion").innerHTML = text
+ipcRenderer.on('message', function(event, text) {
+	document.getElementById('appVersion').innerHTML = text
+	document.getElementById('nodeVersion').innerHTML = text
+	document.getElementById('electronVersion').innerHTML = text
 })
 
 /*****************************************************/
@@ -117,39 +105,37 @@ ipcRenderer.on("message", function (event, text) {
 
 // set host
 const setSysHost = () => {
-  hostvalue = document.getElementById("ipaddress").value
-  if (!hostvalue) alert("Please input host url")
-  else {
-    stg.setItem("sysHost", hostvalue)
+	hostvalue = document.getElementById('ipaddress').value
+	if (!hostvalue) alert('Please input host url')
+	else {
+		stg.setItem('sysHost', hostvalue)
 
-    alert(`Server change to ${hostvalue} Please restart app`)
-  }
+		alert(`Server change to ${hostvalue} Please restart app`)
+	}
 }
 
 // set notification
 const setNotificationHost = () => {
-  notificationHost = document.getElementById("notificationHost").value
-  notificationPort = document.getElementById("notificationPort").value
-  if (!notificationHost) alert("Please input with valid IP")
-  else if (!notificationPort) alert("Please input with valid Port")
-  else {
-    stg.setItem("notificationHost", dNotificationHost)
-    stg.setItem("notificationPort", dNotificationPort)
+	notificationHost = document.getElementById('notificationHost').value
+	notificationPort = document.getElementById('notificationPort').value
+	if (!notificationHost) alert('Please input with valid IP')
+	else if (!notificationPort) alert('Please input with valid Port')
+	else {
+		stg.setItem('notificationHost', dNotificationHost)
+		stg.setItem('notificationPort', dNotificationPort)
 
-    alert(
-      `Server change to ${notificationHost}:${notificationPort} Please restart app`
-    )
-  }
+		alert(`Server change to ${notificationHost}:${notificationPort} Please restart app`)
+	}
 }
 
 // set weather
 const setWeatherKey = () => {
-  hostvalue = document.getElementById("weatherKey").value
-  if (!weatherKey) alert("Please input host url")
-  else {
-    stg.setItem("weatherapi", weatherKey)
-    alert(`Server change to ${weatherKey} Please restart app`)
-  }
+	hostvalue = document.getElementById('weatherKey').value
+	if (!weatherKey) alert('Please input host url')
+	else {
+		stg.setItem('weatherapi', weatherKey)
+		alert(`Server change to ${weatherKey} Please restart app`)
+	}
 }
 
 /*****************************************************/
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+'use strict'
 /****************************************************
  * Module: preload.js
  * Type: Java Script
@@ -5,59 +6,58 @@
  * Date: Oct 2021
  * Description: Modules to control application life and create native browser window
  *****************************************************/
-const { app, BrowserWindow, ipcMain } = require("electron")
+
+const { app, BrowserWindow, ipcMain } = require('electron')
+
 let win
-const path = require("path")
-const { exec } = require("child_process")
-const { autoUpdater } = require('electron-updater');
+
+const path = require('path')
+
+const { exec } = require('child_process')
+
+const { autoUpdater } = require('electron-updater')
 
 function createWindow() {
-  // Create the browser window.
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    frame: true,
-    icon: __dirname + "/logo.ico",
-    webPreferences: {
-      plugins: true,
-      preload: path.join(__dirname, "preload.js"),
-      nodeIntegration: true,
-      enableRemoteModule: true,
-      contextIsolation: false,
-      autoHideMenuBar: true,
-    },
-  })
+	// Create the browser window.
+	win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		frame: true,
+		icon: __dirname + '/logo.ico',
+		webPreferences: {
+			plugins: true,
+			preload: path.join(__dirname, 'preload.js'),
+			nodeIntegration: true,
+			enableRemoteModule: true,
+			contextIsolation: false,
+			autoHideMenuBar: true
+		}
+	})
+	/***************************
+   * Remove MenuBar */
+	// win.setMenuBarVisibility(false)
+	// win.setAutoHideMenuBar(true)
 
-/***************************
-* Remove MenuBar */
+	/****************************/
+	// win.webContents.openDevTools()
 
-  // win.setMenuBarVisibility(false)
-  // win.setAutoHideMenuBar(true)
+	win.loadFile(path.join(__dirname, 'index.html'))
+	/* Call App Upadate */
 
-/****************************/
-  // win.webContents.openDevTools()
-  win.loadFile(path.join(__dirname, "index.html"))
-
-  /* Call App Upadate */
-  win.once('ready-to-show', () => {
-    autoUpdater.checkForUpdatesAndNotify();
-  });
-
-
-
-}
-
-// This method will be called when Electron has finished
+	win.once('ready-to-show', () => {
+		autoUpdater.checkForUpdatesAndNotify()
+	})
+} // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
 
-  app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+app.whenReady().then(() => {
+	createWindow()
+	app.on('activate', function() {
+		// On macOS it's common to re-create a window in the app when the
+		// dock icon is clicked and there are no other windows open.
+		if (BrowserWindow.getAllWindows().length === 0) createWindow()
+	})
 })
 /* 
 Quit when all windows are closed, except on macOS. There, it's common 
@@ -65,53 +65,36 @@ for applications and their menu bar to stay active until the user quits
 explicitly with Cmd + Q.
 */
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit()
-  }
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
 
 const callProgram = (path) => {
-  console.log("accessed")
-  exec(path)
-  // Double quotes are used so that the space in the path is not interpreted as
-  // a delimiter of multiple arguments.
-  console.log("done")
-}
+	console.log('accessed')
+	exec(path) // Double quotes are used so that the space in the path is not interpreted as
+	// a delimiter of multiple arguments.
 
+	console.log('done')
+}
 /***************************
 * App Upadate */
 
 ipcMain.on('app_version', (event) => {
-  event.sender.send('app_version', { version: app.getVersion() });
-});
-
+	event.sender.send('app_version', {
+		version: app.getVersion()
+	})
+})
 autoUpdater.on('update-available', () => {
-  console.log("update found")
-  win.webContents.send('update_available');
-});
-
+	console.log('update found')
+	win.webContents.send('update_available')
+})
 autoUpdater.on('update-downloaded', () => {
-  win.webContents.send('update_downloaded');
-});
-
+	win.webContents.send('update_downloaded')
+})
 ipcMain.on('restart_app', () => {
-  autoUpdater.quitAndInstall();
-});
-
+	autoUpdater.quitAndInstall()
+})
 /****************************/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //Tanbin Hassan Bappi
